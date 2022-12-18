41a718e5f29124f7c53efb1aadb2146399851d26
WenxuC <wenx.chen@outlook.com>
2022-12-17 21:42:26 -0500

    added delete and fixed rendering issues when  adding/deleting items

--- fridge_app/items/urls.py
+++ fridge_app/items/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ItemView, CreateItemView, GetItemView
+from .views import ItemView, CreateItemView, GetItemView, UpdateItemView, DeleteItemView
 urlpatterns = [
     path('item', ItemView.as_view()),
     path('createItem', CreateItemView.as_view()),
-    path('getItems', GetItemView.as_view())
+    path('getItems', GetItemView.as_view()),
+    path('updateItem', UpdateItemView.as_view()),
+    path('deleteItem', DeleteItemView.as_view())
 ]
