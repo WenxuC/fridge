c99d5dbbc9d2b8800bbf3e1a943cbf2fced78f5a
WenxuC <wenx.chen@outlook.com>
2022-12-28 00:10:09 -0500

    static

--- fridge_app/fridge_app/settings.py
+++ fridge_app/fridge_app/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from datetime import timedelta
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -156,7 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
