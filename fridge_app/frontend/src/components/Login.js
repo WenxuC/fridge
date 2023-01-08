d2c7830ed89f5ac10719d02e2c5d209d511f79ee
WenxuC <wenx.chen@outlook.com>
2023-01-07 22:48:11 -0500

    issue #50

--- fridge_app/frontend/src/components/Login.js
+++ fridge_app/frontend/src/components/Login.js
@@ -1,6 +1,21 @@
 import React, { useState, useContext } from 'react';
-import { Button, Grid, TextField, Typography } from '@mui/material';
+import {
+	Button,
+	Grid,
+	TextField,
+	Typography,
+	Avatar,
+	CssBaseline,
+	Link,
+	Paper,
+	Box,
+	Alert,
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AuthContext from '../context/AuthContext';
+
+const theme = createTheme();
 
 export default function Login() {
 	const { loginUser } = useContext(AuthContext);
@@ -8,7 +23,95 @@ export default function Login() {
 	const [password, setPassword] = useState('');
 
 	return (
-		<Grid container spacing={1} align='center'>
+		<ThemeProvider theme={theme}>
+			<Grid container component='main' sx={{ height: '100vh' }}>
+				<CssBaseline />
+				<Grid
+					item
+					xs={false}
+					sm={4}
+					md={7}
+					sx={{
+						backgroundImage: 'url(https://source.unsplash.com/random/?food)',
+						backgroundRepeat: 'no-repeat',
+						backgroundColor: t =>
+							t.palette.mode === 'light'
+								? t.palette.grey[50]
+								: t.palette.grey[900],
+						backgroundSize: 'cover',
+						backgroundPosition: 'center',
+					}}
+				/>
+				<Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+					<Box
+						sx={{
+							my: 8,
+							mx: 4,
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center',
+						}}
+					>
+						<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+							<LockOutlinedIcon />
+						</Avatar>
+						<Typography component='h1' variant='h5'>
+							Sign In
+						</Typography>
+
+						<Box>
+							<TextField
+								margin='normal'
+								required
+								fullWidth
+								label='Username'
+								placeholder='User Name'
+								value={username}
+								onChange={e => setUsername(e.target.value)}
+								autoFocus
+							/>
+
+							<TextField
+								margin='normal'
+								required
+								fullWidth
+								label='Password'
+								value={password}
+								onChange={e => setPassword(e.target.value)}
+								type='password'
+								placeholder='Password'
+							/>
+							<Button
+								type='submit'
+								fullWidth
+								value={JSON.stringify({ username, password })}
+								variant='contained'
+								sx={{ mt: 3, mb: 2 }}
+								onClick={loginUser}
+							>
+								Sign In
+							</Button>
+							<Grid container>
+								<Grid item xs>
+									<Link href='#' variant='body2'>
+										Forgot password?
+									</Link>
+								</Grid>
+								<Grid item>
+									<Link href='/register' variant='body2'>
+										{"Don't have an account?"}
+									</Link>
+								</Grid>
+							</Grid>
+						</Box>
+					</Box>
+				</Grid>
+			</Grid>
+		</ThemeProvider>
+	);
+}
+{
+	/* <Grid container spacing={1} align='center'>
 			<Grid item xs={12}>
 				<Typography variant='h4' component='h4'>
 					Login
@@ -47,6 +150,5 @@ export default function Login() {
 					Sign In
 				</Button>
 			</Grid>
-		</Grid>
-	);
+		</Grid> */
 }
--- fridge_app/frontend/src/components/Register.js
+++ fridge_app/frontend/src/components/Register.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
 import { config } from './Constants';
-import { Button, Grid, TextField, Typography } from '@mui/material';
-import Alert from '@mui/material/Alert';
+import {
+	Button,
+	Grid,
+	TextField,
+	Typography,
+	Avatar,
+	CssBaseline,
+	Link,
+	Paper,
+	Box,
+	Alert,
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const URL = config.url;
+const theme = createTheme();
 
 export default function Register() {
 	const navigate = useNavigate();
@@ -24,71 +37,117 @@ export default function Register() {
 			.post(`${URL}account/register`, body)
 			.catch(function (error) {
 				if (error.response.status === 400) {
-					return getError(error.response.status);
+					return getError('User or email already exists. Please try again.');
 				}
 			})
 			.then(response => {
 				if (response.status === 201) {
 					navigate('/login');
+				} else {
 				}
 			});
 	};
 
 	return (
-		<Grid container spacing={1} align='center'>
-			<Grid item xs={12}>
-				<Typography variant='h4' component='h4'>
-					Register
-				</Typography>
-			</Grid>
-			<Grid item xs={12}>
-				{error !== '' ? <Alert severity='error'>{error}</Alert> : null}
-			</Grid>
+		<ThemeProvider theme={theme}>
+			<Grid container component='main' sx={{ height: '100vh' }}>
+				<CssBaseline />
+				<Grid
+					item
+					xs={false}
+					sm={4}
+					md={7}
+					sx={{
+						backgroundImage: 'url(https://source.unsplash.com/random/?food)',
+						backgroundRepeat: 'no-repeat',
+						backgroundColor: t =>
+							t.palette.mode === 'light'
+								? t.palette.grey[50]
+								: t.palette.grey[900],
+						backgroundSize: 'cover',
+						backgroundPosition: 'center',
+					}}
+				/>
+				<Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+					<Box
+						sx={{
+							my: 8,
+							mx: 4,
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center',
+						}}
+					>
+						<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+							<LockOutlinedIcon />
+						</Avatar>
+						<Typography component='h1' variant='h5'>
+							Sign Up
+						</Typography>
 
-			<Grid item xs={12}>
-				<TextField
-					margin='normal'
-					required
-					label='User Name'
-					placeholder='User Name'
-					value={username}
-					onChange={e => getUserName(e.target.value)}
-					autoFocus
-				/>
+						{error !== '' ? (
+							<Alert fullWidth severity='error'>
+								{error}
+							</Alert>
+						) : null}
+
+						<Box>
+							<TextField
+								margin='normal'
+								required
+								fullWidth
+								label='User Name'
+								placeholder='User Name'
+								value={username}
+								onChange={e => getUserName(e.target.value)}
+								autoFocus
+							/>
+							<TextField
+								margin='normal'
+								required
+								fullWidth
+								label='Email'
+								placeholder='Email'
+								value={email}
+								onChange={e => getEmail(e.target.value)}
+								autoFocus
+							/>
+							<TextField
+								margin='normal'
+								required
+								fullWidth
+								label='Password'
+								value={password}
+								onChange={e => getPassword(e.target.value)}
+								type='password'
+								placeholder='Password'
+							/>
+							<Button
+								type='submit'
+								fullWidth
+								value='sign up'
+								variant='contained'
+								sx={{ mt: 3, mb: 2 }}
+								onClick={handleSubmit}
+							>
+								Sign Up
+							</Button>
+							<Grid container>
+								<Grid item xs>
+									<Link href='#' variant='body2'>
+										Forgot password?
+									</Link>
+								</Grid>
+								<Grid item>
+									<Link href='/login' variant='body2'>
+										{'Already have an account?'}
+									</Link>
+								</Grid>
+							</Grid>
+						</Box>
+					</Box>
+				</Grid>
 			</Grid>
-			<Grid item xs={12}>
-				<TextField
-					margin='normal'
-					required
-					label='Email'
-					placeholder='Email'
-					value={email}
-					onChange={e => getEmail(e.target.value)}
-					autoFocus
-				/>
-			</Grid>
-			<Grid item xs={12}>
-				<TextField
-					margin='normal'
-					required
-					label='Password'
-					value={password}
-					onChange={e => getPassword(e.target.value)}
-					type='password'
-					placeholder='Password'
-				/>
-			</Grid>
-			<Grid item xs={12}>
-				<Button
-					type='submit'
-					value='Login'
-					variant='contained'
-					sx={{ mt: 3, mb: 2 }}
-					onClick={handleSubmit}
-				>
-					Sign In
-				</Button>
-			</Grid>
-		</Grid>
+		</ThemeProvider>
 	);
 }
