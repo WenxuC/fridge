c86d8f3a2630414b0dcdb80101754d8952221a02
WenxuC <wenx.chen@outlook.com>
2023-01-10 15:09:16 -0500

    changed allow_hosts

--- fridge_app/fridge_app/settings.py
+++ fridge_app/fridge_app/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.133.90.253', '127.0.0.1', '.localhost']
+ALLOWED_HOSTS = ['3.136.157.110', '127.0.0.1', '.localhost']
 
 
 # Application definition
@@ -55,7 +55,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=180),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
