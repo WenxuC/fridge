f836e681dc50b4bb5282baeea5af9e1dfb8b9023
WenxuC <wenx.chen@outlook.com>
2023-01-10 20:09:23 -0500

    Update settings.py

--- fridge_app/fridge_app/settings.py
+++ fridge_app/fridge_app/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.136.157.110', '127.0.0.1', '.localhost']
+ALLOWED_HOSTS = ['3.136.157.110', '127.0.0.1', 'www.miseenplace.art', 'miseenplace.art']
 
 
 # Application definition
