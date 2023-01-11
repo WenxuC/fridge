885a34de59b1f25f4c2ae5c6bde4470e17514d44
WenxuC <wenx.chen@outlook.com>
2023-01-10 20:07:57 -0500

    added local_settings import

--- fridge_app/fridge_app/settings.py
+++ fridge_app/fridge_app/settings.py
@@ -162,3 +162,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+try: 
+    from .local_settings import *
+except ImportError:
+    pass
