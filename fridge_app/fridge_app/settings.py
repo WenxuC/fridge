8e2c29cca0771b0822b95ada1ee30c16202b5b64
WenxuC <wenx.chen@outlook.com>
2023-01-10 20:15:23 -0500

    .

--- fridge_app/fridge_app/settings.py
+++ fridge_app/fridge_app/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import timedelta
+from .credentials import DJANGO_SECRET_KEY
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,14 +10,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
+
 # Application definition
+
 INSTALLED_APPS = [
     'corsheaders',
     'django.contrib.admin',
