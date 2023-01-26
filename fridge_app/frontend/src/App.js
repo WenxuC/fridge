c7acf2b2fb2d9e0751ea3e19309d82d2d2c416a3
WenxuC <wenx.chen@outlook.com>
2023-01-25 22:09:42 -0500

    change theme color

--- fridge_app/frontend/src/App.js
+++ fridge_app/frontend/src/App.js
@@ -1,6 +1,8 @@
 // import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from '@mui/material';
+import theme from './components/ui/Theme';
 // Components
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
@@ -9,15 +11,17 @@ import Landing from './components/Landing';
 
 export default function App() {
 	return (
-		<Router>
-			<AuthProvider>
-				<Routes>
-					<Route path='/' element={<Landing />} />
-					<Route path='/dashboard' element={<Dashboard />} />
-					<Route path='/login' element={<Login />} />
-					<Route path='/register' element={<Register />} />
-				</Routes>
-			</AuthProvider>
-		</Router>
+		<ThemeProvider theme={theme}>
+			<Router>
+				<AuthProvider>
+					<Routes>
+						<Route path='/' element={<Landing />} />
+						<Route path='/dashboard' element={<Dashboard />} />
+						<Route path='/login' element={<Login />} />
+						<Route path='/register' element={<Register />} />
+					</Routes>
+				</AuthProvider>
+			</Router>
+		</ThemeProvider>
 	);
 }
---
+++ fridge_app/frontend/src/components/ui/Theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#357a38',
+		},
+		secondary: {
+			main: '#b28704',
+		},
+	},
+});
+
+export default theme;
